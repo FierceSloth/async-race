9126ff699f0c529571ff67b282b06bb82fabfb25
Dastan Hairushev <hairushevdastan@gmail.com>
2026-01-26 18:46:27 +0500

    feat: implement placeholder winners page layout

--- src/pages/winners-page/winners-page.ts
+++ src/pages/winners-page/winners-page.ts
@@ -1,16 +1,32 @@
-import type { Component } from '@common/component';
+import { pageMessages } from '@/common/constants/messages';
+import { Component } from '@common/component';
 
-// import styles from './winners-page.module.scss';
+import styles from './winners-page.module.scss';
+import { Pagination } from '@/components/features/pagination/pagination';
+import { WinnerModal } from '@/components/features/winner-modal/winner-modal';
 
 export class WinnersPage {
   private container: Component;
+  private pagination!: Pagination;
 
   constructor(container: Component) {
     this.container = container;
   }
 
   public render(): void {
-    this.container.node.innerHTML = '<h1> Winner Page </h1>';
+    const mainTitle = new Component({ tag: 'h1', className: styles.mainTitle, text: pageMessages.winner });
+
+    this.pagination = new Pagination({ className: [styles.pagination] });
+    this.pagination.updatePageCounter(1, 1); // ? stub implementation
+
+    const modal = new WinnerModal({ className: [styles.modal] });
+    modal.open(
+      () => {},
+      undefined,
+      'Winner page is not implemented and most likely won’t be added due to tight deadlines 🏗️🚧⏳'
+    ); /// ? stub implementation
+
+    this.container.appendChildren([mainTitle, this.pagination]);
   }
 
   public destroy(): void {}
