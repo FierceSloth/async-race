0b7ba2a47ec766a7dd4c648fecc509d70b56092e
Dastan Hairushev <hairushevdastan@gmail.com>
2026-03-21 00:41:35 +0500

    feat: update base path and add favicon

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename);
 const PORT = 7777;
 
 export default defineConfig({
-  base: '/fiercesloth-JSFE2025Q3/async-race/',
+  base: '/async-race/',
   build: {
     outDir: 'dist',
   },
